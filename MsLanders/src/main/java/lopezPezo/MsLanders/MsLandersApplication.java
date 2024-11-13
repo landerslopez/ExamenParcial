80f4019cfb8bcb69fdebe74543a5089d442436de
landerslopez <116610569+landerslopez@users.noreply.github.com>
2024-11-13 16:49:45 -0500

    Initial commit

---
+++ MsLanders/src/main/java/lopezPezo/MsLanders/MsLandersApplication.java
@@ -0,0 +1,13 @@
+package lopezPezo.MsLanders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLandersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLandersApplication.class, args);
+	}
+
+}
---
+++ MsLanders/src/test/java/lopezPezo/MsLanders/MsLandersApplicationTests.java
@@ -0,0 +1,13 @@
+package lopezPezo.MsLanders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLandersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
