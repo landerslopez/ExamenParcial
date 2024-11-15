77b9a31b75a5e3f3dd74c37808af67e28690cfed
landerslopez <116610569+landerslopez@users.noreply.github.com>
2024-11-15 15:44:22 -0500

    add methods
    Agregar metodos para la gestion de alumno

--- MsLanders/src/main/java/lopezPezo/MsLanders/services/AlumnoService.java
+++ MsLanders/src/main/java/lopezPezo/MsLanders/services/AlumnoService.java
@@ -1,5 +1,41 @@
 package lopezPezo.MsLanders.services;
 
-public class AlumnoService {
-    
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lopezPezo.MsLanders.model.AlumnoModel;
+import lopezPezo.MsLanders.repository.IAlumnoRepository;
+
+@Service
+public class AlumnoService implements IAlumnoService {
+    @Autowired
+    IAlumnoRepository repository;
+
+    @Override
+    public List<AlumnoModel> findAll() {
+        return (List<AlumnoModel>)repository.findAll();
+    }
+
+    @Override
+    public AlumnoModel findById(Integer id) {
+        return (AlumnoModel)repository.findById(id).get();
+    }
+
+    @Override
+    public AlumnoModel add(AlumnoModel model) {
+        return repository.save(model);
+    }
+
+    @Override
+    public AlumnoModel update(AlumnoModel model) {
+        return repository.save(model);
+    }
+
+    @Override
+    public Boolean delete(Integer id) {
+        repository.deleteById(id);
+        return true;
+    }
 }
--- MsLanders/src/main/java/lopezPezo/MsLanders/services/IAlumnoService.java
+++ MsLanders/src/main/java/lopezPezo/MsLanders/services/IAlumnoService.java
@@ -1,5 +1,18 @@
 package lopezPezo.MsLanders.services;
 
-public class IAlumnoService {
-    
+import java.util.List;
+
+import lopezPezo.MsLanders.model.AlumnoModel;
+
+public interface IAlumnoService {
+
+    public List<AlumnoModel> findAll();
+
+    public AlumnoModel findById(Integer id);
+
+    public AlumnoModel add(AlumnoModel model);
+
+    public AlumnoModel update(AlumnoModel model);
+
+    public Boolean delete(Integer id);    
 }
