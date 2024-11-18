f34ffb52ecca1bec726b01fb69d25e6d5940eab0
landerslopez <116610569+landerslopez@users.noreply.github.com>
2024-11-18 17:25:53 -0500

    Update AlumnoModel.java
    Me falto importar

--- MsLanders/src/main/java/lopezPezo/MsLanders/model/AlumnoModel.java
+++ MsLanders/src/main/java/lopezPezo/MsLanders/model/AlumnoModel.java
@@ -8,6 +8,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
