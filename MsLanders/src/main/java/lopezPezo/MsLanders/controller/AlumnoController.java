0773219f3ef0e8a1016196a033c5cb0b7042cf85
landerslopez <116610569+landerslopez@users.noreply.github.com>
2024-11-18 17:33:22 -0500

    Update AlumnoController.java
    update method createAlumno

--- MsLanders/src/main/java/lopezPezo/MsLanders/controller/AlumnoController.java
+++ MsLanders/src/main/java/lopezPezo/MsLanders/controller/AlumnoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -36,9 +37,9 @@ public class AlumnoController {
     
 
     @PostMapping("/create")
-    public AlumnoModel create(@RequestBody AlumnoModel model)
-    {   
-        return alumnoService.addStudent(model);
+    public String createAlumno(@ModelAttribute(name="alumno") AlumnoModel model){   
+        alumnoService.addStudent(model);
+        return "redirect:/alumno/listAlumno";
     }
 
     // findById
