913ebfa14d8d6f2ce817684a74e2deeb151d1a6e
landerslopez <116610569+landerslopez@users.noreply.github.com>
2024-11-18 17:41:00 -0500

    change interfaces
    change crudrepository for jparepository ( more features)

--- MsLanders/src/main/java/lopezPezo/MsLanders/repository/IAlumnoRepository.java
+++ MsLanders/src/main/java/lopezPezo/MsLanders/repository/IAlumnoRepository.java
@@ -1,11 +1,11 @@
 package lopezPezo.MsLanders.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import lopezPezo.MsLanders.model.AlumnoModel;
 
 @Repository
-public interface IAlumnoRepository extends CrudRepository<AlumnoModel, Integer>{
+public interface IAlumnoRepository extends JpaRepository<AlumnoModel, Integer>{
     
 }
