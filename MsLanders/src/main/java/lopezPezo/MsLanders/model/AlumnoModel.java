a3711d0cab5a014b8ad98024bbe3a981ee21c503
landerslopez <116610569+landerslopez@users.noreply.github.com>
2024-11-19 00:40:52 -0500

    change version validation

--- MsLanders/src/main/java/lopezPezo/MsLanders/model/AlumnoModel.java
+++ MsLanders/src/main/java/lopezPezo/MsLanders/model/AlumnoModel.java
@@ -2,14 +2,13 @@ package lopezPezo.MsLanders.model;
 
 import java.math.BigDecimal;
 
-import org.antlr.v4.runtime.misc.NotNull;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
