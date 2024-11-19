7e7047ea7ad83ce64cf42be4bf402ffbbf7085e8
landerslopez <116610569+landerslopez@users.noreply.github.com>
2024-11-19 14:47:17 -0500

    add log

--- MsLanders/src/main/java/lopezPezo/MsLanders/controller/AlumnoController.java
+++ MsLanders/src/main/java/lopezPezo/MsLanders/controller/AlumnoController.java
@@ -1,5 +1,7 @@
 package lopezPezo.MsLanders.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,8 +25,11 @@ public class AlumnoController {
     @Autowired
     private AlumnoServiceImpl alumnoService;
 
+    private static final Logger logger = LoggerFactory.getLogger(AlumnoController.class);
+
     @GetMapping("/listAlumno")
     public String listStudents(Model mav) {
+        logger.info("Listando todos los alumnos");
         mav.addAttribute("alumnos", alumnoService.findAllStudent());
         mav.addAttribute("alumno", new AlumnoModel());
         mav.addAttribute("accion", "/alumno/create");
@@ -37,6 +42,7 @@ public class AlumnoController {
 
         if (bindingResult.hasErrors()) {
             mav.addAttribute("alumnos", alumnoService.findAllStudent());
+            logger.info("Agregando alumnos");
             mav.addAttribute("accion", "/alumno/create");
             return "listAlumno";
         }
@@ -49,6 +55,7 @@ public class AlumnoController {
     public String findById(@PathVariable(name = "id") String id, Model mav) {
         mav.addAttribute("alumnos", alumnoService.findAllStudent());
         AlumnoModel u = alumnoService.findByIdStudent(Integer.parseInt(id));
+        logger.info("Buscando alumnos");
         mav.addAttribute("alumno", u);
         mav.addAttribute("accion", "/alumno/update");
         return "listAlumno";
@@ -61,6 +68,7 @@ public class AlumnoController {
                 
         if (bindingResult.hasErrors()) {
             mav.addAttribute("alumnos", alumnoService.findAllStudent());
+            logger.info("Editando alumno");
             mav.addAttribute("accion", "/alumno/update");
             return "listAlumno";
         }
@@ -71,6 +79,7 @@ public class AlumnoController {
     // delete
     @GetMapping("/delete/{id}")
     public String deleteAlumno(@PathVariable(name = "id") String id) {
+        logger.info("Elimiando alumno");
         alumnoService.deleteStudent(Integer.parseInt(id));
         return "redirect:/alumno/listAlumno";
     }
